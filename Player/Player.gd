179ce16eff22d347cc5f86a2a690275b15e5efc4
Selhar <selhar@protonmail.com>
2018-11-29 12:46:21 -0300

    movement

---
+++ Player/Player.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+export (int) var speed
+var velocity = Vector2()
+var screensize = Vector2(480, 720)
+
+func _ready():
+	pass
+
+func _process(delta):
+	get_input()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = "idle"
+
+func get_input():
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 120
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 120
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 120
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 120
+	if velocity.length() > 0:
+		velocity.normalized() * speed
